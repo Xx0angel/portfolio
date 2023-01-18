48178266e617f302fec0d4cbc448048f57db5561
Angel Garcia <angeliux.jossue@gmail.com>
2023-01-18 03:49:16 -0600

    skills mockup

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ menu.onclick = () => {
     console.log("click")
     menuBar.classList.toggle("active__menu");
 }
+
+// SKILLS 
+const skillsContent = document.querySelector(".skills__item"),
+      skillsHeader = document.querySelectorAll(".skill")
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__item skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__item skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    console.log()
+    el.addEventListener('click', toggleSkills)
+})
+
